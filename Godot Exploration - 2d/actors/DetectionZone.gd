325c001aa16bdf9b8009e24d8bf08d2478a3327c
JoshuaMoelans <joshua.moelans@student.uantwerpen.be>
2024-05-29 11:43:41 +0200

    Improved circular vision cone

--- Godot Exploration - 2d/actors/DetectionZone.gd
+++ Godot Exploration - 2d/actors/DetectionZone.gd
@@ -2,11 +2,22 @@ extends Area2D
 
 @onready var visionCone = $CollisionPolygon2D
 
-func setCone(angle, distance):
-	# sets CollisionPolygon2D dimensions based on a given vision angle and view distance
-	var x_cord = distance
-	var y_cord = tan(angle/2)*distance
-	var start = Vector2(0,0)
-	var top = Vector2(x_cord, y_cord)
-	var bot = Vector2(x_cord, -y_cord)
-	visionCone.polygon = PackedVector2Array([start, top, bot])
+func setCone(angle, distance, resolution=30):
+	# Convert angle from degrees to radians
+	angle = angle * PI / 180
+	var half_angle = angle / 2
+	var step = angle / resolution
+
+	var points = PackedVector2Array()
+	points.append(Vector2(0, 0))  # Start point
+
+	# Generate points for the vision cone
+	for i in range(resolution + 1):
+		var theta = -half_angle + step * i
+		var x = distance * cos(theta)
+		var y = distance * sin(theta)
+		points.append(Vector2(x, y))
+
+	points.append(Vector2(0, 0))  # End point
+
+	visionCone.polygon = points
